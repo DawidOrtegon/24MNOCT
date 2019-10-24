ff2937bf25b74d9ffc95c962ad688a9ceb0ee10c
unknown <bogusz.pawlowski@gmail.com>
2019-10-24 08:49:42 +0400

    Initial commit

---
+++ src/main/java/edu/mirek/SinusFX.java
@@ -0,0 +1,16 @@
+package edu.mirek;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SinusFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
